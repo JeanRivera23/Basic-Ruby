1ab3ca83b1016dbf50615fa108a7d8592492dbe0
Jeanette Rivera <jeanetterivera465@gmail.com>
2017-04-15 18:33:59 -0400

    finally done

--- basicRuby.rb
+++ basicRuby.rb
@@ -1,41 +1,33 @@
 #Create a function that takes a string as an argument and adds the phrase "Only in America!" to the end of it
 
+
+#function "america" accepts one argument, "a" and concatenates "a" with the string " Only in America"
 def america(a)
   a + " Only in America."
 end
-
+# this calls/invokes on function "america", passes in "a" a string argument and outputs the result
 puts america("Pizza is great.")
 
 
 # Create a function to find the maximum value in an array of numbers. For instance: [100,10,-1000] should return 100. Do not use Ruby's built-in .max function.
 
-# num_list = [234, 689, 12345, 900]
-#
-# def max_val
-#   for i in num_list
-#     if num_list[i] > num_list[i + 1]
-#       puts i
-#     end
-#   end
-# end
 
 def max_val
-  num_list = [234, 689, 12345, 900]
-  x = 0
+  # array of numbers
+  num_list = [907, 689, 12345, 900]
 
-  for i in num_list
-    #if num_list[x] > num_list[x+1]
-    puts num_list[x]
+  # sorted array of numbers stored in sorted_list variable, sorts by small to large
+  sorted_list = num_list.sort
+  # since arrays start index count at 0 and array lenghts start at 1, to get position of the last number in array (the biggest number) length needs to be subtracted by 1 to get the array index
+  i = sorted_list.length - 1
+  # stores the value of the last item in array in magic_num variable
+  magic_num = sorted_list[i]
+  # displays that number
+  puts magic_num
 end
+# calls/invokes function
+max_val
 
-
-def max_val
-  num_list = [234, 689, 12345, 900]
-
-  if num_list[x] > num_list[x+1]
-    puts num_list[x]
-  end
-end
 
 # Create a function that takes two arguments - both of them arrays. Inside of the function, combine the arrays using the items from the first array as keys and the second array as values. Do not use Ruby's built-in .zip function. For example, when these two arrays are supplied as arguments:
 #
@@ -45,16 +37,24 @@ end
 #
 #     {toyota: "Prius", tesla: "Model S"}
 
-def combo
-  array_pies = ["Apple Pie", "Coconut Custard Pie", "Key Lime Pie"]
-  array_fillings = ["McIntosh Apples", "Coconuts", "Key Limes"]
+def autumn_storage(a_one, a_two)
+  # create empty hash
+  favorite_season = {}
+  # counter
+  i = 0
 
-  def put_together
-    for i in array_pies
-      array_pies
+  # loops puts array arguments into hash
+  while i <= a_one.length - 1
+    # hash key and value set
+    favorite_season[a_one[i]] = a_two[i]
+    # increments counter
+    i = i + 1
   end
+  #outputs hash
+  puts favorite_season
 end
-
+# invokes function
+autumn_storage([:color, :food, :drink], ["orange", "pumpkin pie", "apple cider"])
 
 # Write a program that prints the numbers from 1 to 100, except:
 #
@@ -62,11 +62,25 @@ end
 # for multiples of five print "Buzz"
 # Print "FizzBuzz" for numbers that are multiples of both 3 and 5.
 
-def fizz_buzz
+def strange
+  # converts range to array and stores in variable "nums"
   nums = (1..100).to_a
-  nums.each do |item|
-    if item = 3
-      puts "Fizz"
-    end
-  end
+
+  # stores weird words in variables for later retrieval
+  a = "Fizz"
+  b = "Buzz"
+  c = "FizzBuzz"
+
+  # checks "nums" array for numbers divisible by 3 & 5 and replaces with c, FizzBuzz
+  fizzy_buzzy = nums.map! {|i| i % 3 == 0 && i % 5 == 0 ? c : i}
+  # checks "nums" array for numbers divisible by 3 and replaces with a, Fizz
+  fizzy = fizzy_buzzy.map! {|i| i % 3 == 0 ? a : i}
+  # checks "nums" array for numbers divisible by 5 and replaces with b, Buzz
+  buzzy = fizzy.map! {|i| i % 5 == 0 ? b : i}
+
+  # converts buzzy array to string and outputs
+  puts buzzy.to_s
 end
+
+# invokes function "strange"
+strange
